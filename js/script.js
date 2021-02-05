d895406fbb9d6bd3113f8cf7f6e0fa5c7a393b88
AdenikeSalau <kifademi@gmail.com>
2021-02-05 02:42:03 +0100

    movie info truncated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const connection = document.querySelector('.connection')
 const list = document.querySelector('.movie-list')
 const search = document.querySelector('input')
 
@@ -11,51 +12,54 @@ const movieApp = async(url) => {
 
     try {
         let res = await axios.get(url)
-        console.log(res)
         let data = await res.data.results
 
-        console.log(data)
-
+        // for each movie..
         data.forEach(movie => {
+            // create tags to hold the movie information
             const el = document.createElement('DIV')
             const image = document.createElement('IMG')
             const title = document.createElement('H2')
             const text = document.createElement('P')
+            const info = document.createElement('DIV')
             const release = document.createElement('SPAN')
 
             el.setAttribute('class', 'movie')
-             image.src = `${imgPath}${movie.poster_path}` 
+            info.setAttribute('class', 'info')
+            image.src = `${imgPath}${movie.poster_path}` 
             title.innerHTML = `${movie.title}` 
             release.innerHTML = `Release Date:${movie.release_date}`
             text.innerHTML = `${movie.overview}`
 
+            connection.style.display = 'none'
+
             el.appendChild(image)
             el.appendChild(title)
             el.appendChild(release)
-            el.appendChild(text)
+            info.appendChild(text)
+            el.appendChild(info)
             list.appendChild(el)
             
         });
     } catch (error) {
         console.log(error)
+        connection.innerHTML = "Please connect to the internet or check your connection."
     }
     
 }
 
-
-
+// search through the page for a keyword when a letter is entered
 search.addEventListener('keyup', (e) => {
     e.preventDefault()
 
     let searchTerm = search.value
     if (searchTerm) { 
           list.innerHTML = ''    
-        console.log(search.value)       
+        console.log(search.value) 
+        connection.style.display = "block"      
         movieApp(searchPath + searchTerm)
         // search.value = ''
-
         console.log(list.length)       
-
     }
 })
 
